0e52c91cb1a9d4b775296c7439088da90fc6770b
Mike Agazarian <mike@Mikes-MacBook-Air.local>
2023-09-22 00:47:04 -0700

    updating chagnes made

--- galeshap.java
+++ galeshap.java
@@ -1,22 +1,27 @@
 import java.util.ArrayList; 
 import java.util.List;
 import java.util.Scanner; 
-
+import java.util.HashMap;
 
 public class galeshap{
 
-// public static void match(List<List<Integer>> man, List<List<Integer>> women, List<Integer> couple, int pairs){
-//     int m = 0;
-//     int w = 0; 
-//     List<Integer> freeMan = new ArrayList<Integer>();
+public static void match(List<List<Integer>> man, List<List<Integer>> women, List<Integer> couple, int pairs){
+    List<Integer> isFree = new ArrayList<Integer>();
+    int m = 0; 
+    int w = 0;
+    int pref = 0;
+    for(int i = 0; i < pairs; i++){
+        isFree.add(i);
+    }
     
-//     for(int i = 0; i < pairs; i++){
-//         freeMan.add(i);
-//     }
-
-//     while(couple.get(m).get(
-//     }
-// }
+    while(isFree != null){
+        w = man.get(m).get(pref);
+        if(isFree.containsValue(w)){
+            if(isFree)
+        }
+        }
+    }
+}
 
 public static List<Integer> createManList(Scanner scan, int pairs){
 
@@ -57,13 +62,8 @@ public static void main(String[] args){
     for(int i = 0; i < pairs; i++){
         women.add(createWomanList(scan, pairs));
     }
-    
-    for(int i = 0; i < pairs; i++){
-        couple.add(i, null);
-    }
 
 
-    //testing the list out
-    System.out.println(men.get(0).get(1));
+
 }
 }
